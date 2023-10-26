cadb22e257be23aacbdc28b7861d24dde2b756cb
TheoHarding <79554009+TheoHarding@users.noreply.github.com>
2023-10-26 18:17:38 +1100

    Moving question 2 from laptop to desktop

---
+++ Question 2.R
@@ -0,0 +1,28 @@
+
+cake <- read.csv("./Data/cake.csv")
+
+
+#Part A
+table(cake$Temp, cake$Recipe)
+
+#temp and recipe do have an impact on the angle the cake broke but the  interaction variable is insignificant 
+
+#It is balanced - there is the same number of subjects in each cohort
+
+#Part B
+boxplot(cake$Angle~cake$Temp)
+
+boxplot(cake$Angle~cake$Recipe)
+
+#Part C
+av = aov(formula = Angle~Recipe*Temp, data = cake)
+
+#Part D
+summary(av)
+plot(av)
+
+#Part E
+me  <- aov(formula = Angle~Recipe+Temp, data = cake)
+summary(me)
+plot(me)
+
